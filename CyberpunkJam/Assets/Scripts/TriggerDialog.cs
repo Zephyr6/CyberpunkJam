4ed330a167d7501a897c99c2432424a2a2a7dc70
Carlos <murlocholmes@gmail.com>
2014-03-03 20:17:41 -0700

    BN-0001 Dialog work.

---
+++ CyberpunkJam/Assets/Scripts/TriggerDialog.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDialog : MonoBehaviour {
+
+    public DialoguerDialogues dialog;
+
+    private OldSchoolRPGDialogueGUI dialogGUI;
+    private bool isActive = true;
+
+	// Use this for initialization
+	void Start () {
+        dialogGUI = GameObject.FindGameObjectWithTag("Dialog").GetComponent<OldSchoolRPGDialogueGUI>();
+	}
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (isActive)
+        {
+            dialogGUI.StartDialog(dialog);
+            isActive = false;
+        }
+    }
+}
