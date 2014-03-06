6237030d82ba86e03b94a57a5076ffd1a02a126c
truffles23 <wailingshadow@gmail.com>
2014-03-05 23:25:39 -0700

    fixed merge conflicts

--- CyberpunkJam/Assets/Scripts/BulletCollider.cs
+++ CyberpunkJam/Assets/Scripts/BulletCollider.cs
@@ -4,44 +4,47 @@ using System.Collections;
 public class BulletCollider : MonoBehaviour
 {
 
-    public Vector2 moveSpeedVector = new Vector2();
+    //public Vector2 moveSpeedVector = new Vector2();
     public float moveSpeed = 1f;
     public int damage = 1;
     public float timeToLive = 2f;
     public bool firedByEnemy = false;
-    private float timer = 0;
     // Use this for initialization
     void Start()
     {
-        timer = timeToLive;
+        var colliders = gameObject.GetComponents<Collider2D>();
+        foreach (var collider in colliders)
+        {
+            if (!firedByEnemy)
+            {
+                var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                foreach (var enemy in enemies)
+                {
+                    
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer <= 0)
-        {
-            DestroyObject(this.gameObject);
-        }
-        else
-        {
-            timer -= Time.deltaTime;
-        }
+        Destroy(gameObject, timeToLive);
     }
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(transform.position.x + moveSpeedVector.x,
-            transform.position.y + moveSpeedVector.y, transform.position.z);
+        //transform.position = new Vector3(transform.position.x + moveSpeedVector.x,
+        //    transform.position.y + moveSpeedVector.y, transform.position.z);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (firedByEnemy && other.tag != "Enemy")
+        if (firedByEnemy && other.tag == "Player")
         {
             DestroyObject(this.gameObject);
         }
-        else if (!firedByEnemy && other.tag != "Player")
+        else if (!firedByEnemy && other.tag == "Enemy")
         {
             DestroyObject(this.gameObject);
         }
--- CyberpunkJam/Assets/Scripts/PlayerGun.cs
+++ CyberpunkJam/Assets/Scripts/PlayerGun.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerGun : MonoBehaviour {
 
-    public Collider2D bullet;
+    public Rigidbody2D bullet;
 
 	// Use this for initialization
 	void Start () {
@@ -20,23 +20,8 @@ public class PlayerGun : MonoBehaviour {
 
     void Fire()
     {
-        int direction = (int)Input.GetAxis("Vertical");
-        Collider2D bulletClone = (Collider2D)Instantiate(bullet, transform.position, transform.rotation);
-        switch (direction)
-        {
-            case 0:
-                if(transform.localScale.x > 0)
-                    bulletClone.GetComponent<BulletCollider>().moveSpeedVector.x = bulletClone.GetComponent<BulletCollider>().moveSpeed;
-                else
-                    bulletClone.GetComponent<BulletCollider>().moveSpeedVector.x = -bulletClone.GetComponent<BulletCollider>().moveSpeed;
-                break;
-            case 1:
-                bulletClone.GetComponent<BulletCollider>().moveSpeedVector.y = bulletClone.GetComponent<BulletCollider>().moveSpeed;
-                break;
-            case -1:
-                bulletClone.GetComponent<BulletCollider>().moveSpeedVector.y = -bulletClone.GetComponent<BulletCollider>().moveSpeed;
-                break;
-        }        
+        Rigidbody2D bulletClone = (Rigidbody2D)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = new Vector2((bulletClone.GetComponent<BulletCollider>().moveSpeed * transform.localScale.x), 0);
         bulletClone.GetComponent<BulletCollider>().firedByEnemy = false;
     }
 }
