b148b1d9e1805c02043d40aaa152c1f52d590477
truffles23 <wailingshadow@gmail.com>
2014-03-08 02:10:46 -0700

    Added a script for player health just the basics for now

---
+++ CyberpunkJam/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthManager : MonoBehaviour {
+
+    public int Health;
+    public bool IsDead = false;
+    public int MaxHealth = 100;
+	// Use this for initialization
+	void Start () {
+        Health = MaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Health <= 0)
+        {
+            IsDead = true;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Projectile")
+        {
+            if (other.GetComponent<BulletCollider>().firedByEnemy)
+            {
+                Health -= other.GetComponent<BulletCollider>().damage;
+            }
+        }
+    }
+}
